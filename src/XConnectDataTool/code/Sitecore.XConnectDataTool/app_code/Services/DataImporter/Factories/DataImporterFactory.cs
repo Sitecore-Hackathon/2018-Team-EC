808efbf69648ef972d6fdcd5c80ca5a16a31f9f2
Richard Leiva <richard.leiva@verndale.com>
2018-03-03 12:41:50 -0500

    Add importer structure

---
+++ src/XConnectDataTool/code/Sitecore.XConnectDataTool/app_code/Services/DataImporter/Factories/DataImporterFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.XConnectDataTool.app_code.Services.DataImporter.Services;
+
+namespace Sitecore.XConnectDataTool.app_code.Services.DataImporter.Factories
+{
+    public class DataImporterFactory
+    {
+        /// <summary>
+        /// Builds a concrete instance of the Wordpresservice repository.
+        /// </summary>
+        public static IDataImporterService Build()
+        {
+            return new DataImporterService();
+        }
+    }
+}
